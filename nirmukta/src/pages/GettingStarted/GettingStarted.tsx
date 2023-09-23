90dbe4e36e47e6a6978994a07d8db0bf4ebcb169
Xq_ankit <guptankit1712004@gmail.com>
2023-09-23 13:11:14 +0530

    tried fixing

--- nirmukta/src/pages/GettingStarted/GettingStarted.tsx
+++ nirmukta/src/pages/GettingStarted/GettingStarted.tsx
@@ -1,9 +1,83 @@
+import { useLogin } from "@refinedev/core";
+import { useEffect, useRef } from "react";
 
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import { ThemedTitleV2 } from "@refinedev/mui";
 
-const GettingStarted = () => {
+//logo...
+// import {#} from "public";
+import { CredentialResponse } from "@mui/x-src-componets-google.d.ts";
+
+export const GettingStarted: React.FC = () => {
+  const { mutate: login } = useLogin<CredentialResponse>();
+
+  const GoogleButton = (): JSX.Element => {
+    const divRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+      if (typeof window === "undefined" || !window.google || !divRef.current) {
+        return;
+      }
+
+      try {
+        window.google.accounts.id.initialize({
+          ux_mode: "popup",
+          client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+          callback: async (res: CredentialResponse) => {
+            if (res.credential) {
+              login(res);
+            }
+          },
+        });
+        window.google.accounts.id.renderButton(divRef.current, {
+          theme: "filled_blue",
+          size: "medium",
+          type: "standard",
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }, []);
+
+    return <div ref={divRef} />;
+  };
+
   return (
-    <div>GettingStarted</div>
-  )
-}
+    <Container
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor:"#2E1A47"
+      }}
+    >
+      <Box
+        sx={{backgroundColor:'#2E1A47'}}
+      >
+        <ThemedTitleV2
+          collapsed={false}
+          wrapperStyles={{
+            fontSize: "22px",
+            justifyContent: "center",
+          }}
+        />
 
-export default GettingStarted
+        <GoogleButton />
+
+        <Typography align="center" color={"text.secondary"} fontSize="12px">
+          Powered by
+          <img
+            alt="Nirmukti logo"
+            src={} //logo
+          />
+          Nirmukti
+         
+        </Typography>
+      </Box>
+    </Container>
+  );
+};
+
